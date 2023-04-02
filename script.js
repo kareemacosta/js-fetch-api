0a80abb594d3fb85797391bf56eb99cdeae62cb3
kareemacosta <97877649+kareemacosta@users.noreply.github.com>
2023-04-02 13:22:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const newUser = {
+    name: 'Maria',
+    job: 'Teacher'
+}
+
+(fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser) 
+}).then(res => {
+        if (!res.ok) {
+            console.log('Problem');
+            return;
+        } 
+        return res.json();
+    }).then(data => {
+            console.log('Success');
+        })
+    .catch(error => {
+        console.log(error);
+    }));
